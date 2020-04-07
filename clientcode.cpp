ef14e2280b0ad723c342cda93c2b45ace17a4fed
DenardYap <49737780+DenardYap@users.noreply.github.com>
2020-04-08 02:42:48 +0800

    Add files via upload

---
+++ clientcode.cpp
@@ -0,0 +1,16 @@
+//clientcode
+#include "SpaceShooter.cpp"
+
+
+int main(int argc, char *argv[]){ //command line arguments
+		Game game;
+		while (game.is_it_running()==true){
+			game.render();
+			game.update();
+			game.handle_event();
+			//run as long as is_running is true
+		}
+		game.clean();
+		cout << "This happened!";
+		
+}
